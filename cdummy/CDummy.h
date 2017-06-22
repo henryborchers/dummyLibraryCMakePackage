16db88ecab30b6385542074f7f7209b711b92ea2
Henry Borchers <henryborchers@yahoo.com>
2017-06-21 21:19:59 -0500

    FIXED right quotes

--- cdummy/CDummy.h
+++ cdummy/CDummy.h
@@ -1,7 +1,7 @@
 #ifndef DUMMY_CLIBRARY_LIBRARY_H
 #define DUMMY_CLIBRARY_LIBRARY_H
 
-#include <cdummy_export.h>
+#include "cdummy_export.h"
 void CDUMMY_EXPORT hello(void);
 int CDUMMY_EXPORT do_something();
 #endif
